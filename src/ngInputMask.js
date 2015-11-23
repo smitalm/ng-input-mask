ffd6fd8e5afe96149090467dc886a4ff749115cf
Matej Šmitala <matej.smitala@gmail.com>
2015-11-23 22:26:06 +0100

    first draft

---
+++ src/ngInputMask.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    var ngInputMask = angular
+        .module('ngInputMask');
+
+    ngInputMask.directive('ngInputMask', directive);
+
+    directive.$inject = ['$compile'];
+    function directive($compile) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: false,
+            link: link
+        };
+
+        function link(scope, elem, attrs) {
+            scope.$watch(attrs.ngModel, updateClass);
+
+            $compile('<label for="'+attrs.id+'">{{' + attrs.ngInputMask + '}}</label>')(scope, function(cloned){
+                elem.after(cloned);
+            });
+
+            function updateClass() {
+                elem.next().attr('class', elem.attr('class') + ' ng-input-mask');
+            }
+        }
+    }
+
+    module.exports = ngInputMask;
+})();
